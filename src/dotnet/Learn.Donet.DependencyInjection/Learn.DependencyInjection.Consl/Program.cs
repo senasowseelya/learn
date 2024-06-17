46fbaff0abc91846f7c16e9bbb2c6a6ceaa12da0
SenaSowseelyaVishnumolakala <senasowseelya.v@technovert.com>
2024-06-17 19:15:19 +0530

    Dependency Injection in Console application

---
+++ src/dotnet/Learn.Donet.DependencyInjection/Learn.DependencyInjection.Consl/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Learn.Dotnet.DependencyInjection.Cnsl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Install Dependency Injection Nuget Package
+            //Create Service Collection
+            ServiceCollection serviceCollection = new();
+            //Register Service to Service Collection
+            serviceCollection.AddTransient<IGreet, GreetService>();
+            //Build Service Provider
+            ServiceProvider sp = serviceCollection.BuildServiceProvider();
+            //Resolve Service
+            IGreet? service = sp.GetService<IGreet>();
+            if (service != null)
+            {
+                service.Greet();
+            }
+            else
+            {
+                Console.WriteLine("Service resolution failed. Service is null.");
+            }
+        }
+    }
+
+
+    public interface IGreet
+    {
+        public void Greet();
+    }
+    public class GreetService : IGreet
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Hello, World! from Greeting Service");
+        }
+    }
+
+
+}
