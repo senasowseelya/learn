3218e905967c25a7fa96ab42059cb47bc0139313
SenaSowseelyaVishnumolakala <senasowseelya.v@technovert.com>
2024-07-25 12:24:13 +0530

    conc:  Server and Client Socket communication

---
+++ src/dotnet/Learn.Dotnet.Sockets/Learn.Dotnet.ClientSocket/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Learn.Dotnet.ClientSocket
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            IPAddress[] iPAddresses = Dns.GetHostAddresses(hostName);
+            IPAddress localhost = iPAddresses[0];
+            IPEndPoint serverEndPoint = new(localhost,11011);
+            Socket clientSocket = new(localhost.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            await clientSocket.ConnectAsync(serverEndPoint);
+            Console.WriteLine("Connection Request Sent");
+            while (true)
+            {
+                Console.WriteLine("Press enter to send data to server");
+                Console.ReadKey();
+                int bytesSent = await clientSocket.SendAsync(Encoding.ASCII.GetBytes("Hi, This is message from client <|EOM|>"));
+                Console.WriteLine("Data is sent from client");
+                byte[] buffer = new byte[1024];
+                int bytesReceived = await clientSocket.ReceiveAsync(buffer);
+                Console.WriteLine($"Ack message {Encoding.ASCII.GetString(buffer,0,bytesReceived)}");
+            }
+        }
+    }
+}
---
+++ src/dotnet/Learn.Dotnet.Sockets/Learn.Dotnet.ServerSocket/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Dynamic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Learn.Dotnet.ServerSocket
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await GetServerSocketReady();
+        }
+
+        public static async Task GetServerSocketReady()
+        {
+            string hostName = Dns.GetHostName();
+            IPAddress[] ipAddresses = await Dns.GetHostAddressesAsync(hostName);
+            IPAddress localhost = ipAddresses[0];
+            int port = 11011;
+            IPEndPoint endpoint = new(localhost, port);
+            Socket serverSocket = new(localhost.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(endpoint);
+            serverSocket.Listen();
+            Console.WriteLine("Server is Listening");
+            Socket handler = await serverSocket.AcceptAsync();
+            Console.WriteLine("Accepted Connection");
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int numberOfBytesReceived = await handler.ReceiveAsync(buffer);
+                string response = Encoding.ASCII.GetString(buffer, 0, numberOfBytesReceived);
+                if(response.IndexOf("|EOM|") > -1) // indicates end of message.
+                {
+                    Console.WriteLine($"Data Received from client : {response}");
+                    Console.WriteLine("Press enter to send acknowledgement to client");
+                    Console.ReadKey();
+                    await handler.SendAsync(Encoding.ASCII.GetBytes($"|ACK| from Server : Received data of bytes{numberOfBytesReceived}"));
+                }
+               
+            }
+
+
+        }
+    }
+}
