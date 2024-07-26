ad2692ef00bd264a72b13f2465913902f64de7dc
Lohitha P <Lohitha.P@cdsgroups.com>
2024-07-26 12:35:45 +0530

    conc: HttpListener

---
+++ src/dotnet/Learn.Dotnet.HttpListener/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Text;
+namespace Learn.Dotnet.HttpListener
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.HttpListener  listener= new System.Net.HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            System.Console.WriteLine("Listening...");
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            Console.WriteLine("Request Url"+ request.Url);
+            HttpListenerResponse response = context.Response;
+            Console.WriteLine(response.OutputStream + "Output Stream");
+            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] bytes = Encoding.ASCII.GetBytes(responseString);
+            response.ContentLength64 = bytes.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(bytes, 0, bytes.Length);
+            listener.Stop();
+            Console.Read();
+        }
+    }
+}
